771f5827b1a8db76e1ed7f16c5e4dcfce34d4228
David Papuc <davidpapuc05@gmail.com>
2020-11-15 13:11:47 +0200

    Add files via upload

---
+++ count_vectorizer_example.py
@@ -0,0 +1,24 @@
+
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+text = [
+    'This is the first phrase is',
+    'This is the second one',
+    'And this is the last',
+]
+test_text = ['My name is Robert']
+
+count_vect = CountVectorizer(lowercase=True, analyzer='word')
+count_vect.fit(text)  # learn the vocabulary
+print(count_vect.vocabulary_)
+
+X = count_vect.transform(text)  # sparse matrix
+
+X = X.toarray()  # numpy matrix
+print(X)
+
+print(count_vect.get_feature_names())
+
+X_test = count_vect.transform(test_text)
+print(X_test.toarray())
